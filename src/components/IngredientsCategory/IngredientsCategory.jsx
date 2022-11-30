022b5b69bb8667ae4cfde8eae03fec92303e0968
Ivan <foxriver660@gmail.com>
2022-11-30 14:00:56 +0300

    feature-add modal to BurgIng

--- src/components/IngredientsCategory/IngredientsCategory.jsx
+++ src/components/IngredientsCategory/IngredientsCategory.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import IngredientCard from "../IngredientCard/IngredientCard";
+import IngredientDetails from "../IngredientDetails/IngredientDetails";
+import classes from './IngredientsCategory.module.css';
 
 const IngredientsCategory = ({ filteredArr }) => {
+  const [openCard, setOpenCard] = React.useState(false);
+  const [selectedIngredient, setSelectedIngredient] = React.useState();
+  const escClose = (e) => {
+    if (e.key === "Escape") {
+      setOpenCard(false);
+    }
+  };
+  React.useEffect(() => {
+    window.addEventListener("keydown", escClose);
+    return () => window.removeEventListener("keydown", escClose);
+  }, [openCard]);
+
+
   return (
     <>
       {filteredArr.map((item) => (
-        <li key={item._id}>
+        <li className={classes.card} key={item._id} onClick={() => {
+          setSelectedIngredient(item);
+          setOpenCard(true);
+        }}>
           <IngredientCard
             name={item.name}
             _id={item._id}
@@ -16,6 +34,14 @@ const IngredientsCategory = ({ filteredArr }) => {
           />
         </li>
       ))}
+      {selectedIngredient && (
+        <IngredientDetails
+          data={selectedIngredient}
+          onKeyPress={escClose}
+          open={openCard}
+          onClose={() => setOpenCard(false)}
+        />
+      )}
     </>
   );
 };
