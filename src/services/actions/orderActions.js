5ea30e33cb275e3b6d9a4114d065d471f962dbf3
Ivan <foxriver660@gmail.com>
2023-02-12 16:18:49 +0300

    release 2.2

--- src/services/actions/orderActions.js
+++ src/services/actions/orderActions.js
@@ -9,7 +9,7 @@ export const getApiOrder = (ingredients, token) => (dispatch) => {
   getOrderAPI(ingredients, token)
     .then((res) => {
       dispatch({ type: GET_ORDER, payload: res.order.number });
-      console.log("ДАННЫЕ НАПРАВЛЕНЫ getApiOrder:", res);
+      /* console.log("ДАННЫЕ НАПРАВЛЕНЫ getApiOrder:", res); */
     })
     .catch((err) => {
       console.log(err);
