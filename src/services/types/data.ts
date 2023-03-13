99fef33e01c0b7baa9345a6977b99c8293eea2aa
Ivan <foxriver660@gmail.com>
2023-03-13 22:20:29 +0300

    fix problems with types

--- src/services/types/data.ts
+++ src/services/types/data.ts
@@ -29,7 +29,21 @@ export type TIngredient = {
   readonly _id: string;
   readonly nanoid?: string;
 };
-
+export type TIngredientConst = {
+  readonly calories?: number;
+  readonly carbohydrates?: number;
+  readonly fat?: number;
+  readonly image?: string;
+  readonly image_large?: string;
+  readonly image_mobile?: string;
+  readonly name?: string;
+  readonly price?: number;
+  readonly proteins?: number;
+  readonly type?: string;
+  readonly __v?: number;
+  readonly _id?: string;
+  readonly nanoid: string;
+};
 export type TUser = {
   readonly email: string;
   readonly name: string;
